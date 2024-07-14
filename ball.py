50286c8f63b9960768595226b4aa348b7337e606
James Kelly <jim.m.kelly@gmail.com>
2024-07-14 17:40:16 +0200

    Refactoring bounce function

--- ball.py
+++ ball.py
@@ -6,59 +6,58 @@ from settings import *
 
 class Ball:
     def __init__(self, x, y, r, w, colour):
-        self.direction = Vector2((random.randint(-5,5)),(random.randint(-5,5)))
         self.pos = Vector2(x,y)
+        self.velocity = Vector2((random.randint(-5,5)),(random.randint(-5,5)))
         self.r = r
         self.w = w
-        self.volume = 3.14 * r**2
-        self.velocity = 2
+        self.mass = 3.14 * r**2
+        self.acceleraton = 1
         self.colour = colour
         self.gravity = GRAVITY
     
     def draw(self,screen):
         pygame.draw.circle(screen, (self.colour), (self.pos), self.r, self.w)
 
-    def move(self):
+    def update(self):
         self.apply_gravity()
-        self.pos.x += self.direction.x * self.velocity
-        self.pos.y += self.direction.y * self.velocity
-    
-    def bounce(self,other):
-        new_velocity = ((self.volume - other.volume)/(self.volume + other.volume))*self.velocity + ((2*other.volume) / (self.volume + other.volume)) * other.velocity
-        new_velocity2 = ((2*self.volume)/(self.volume + other.volume))*self.velocity - ((self.volume - other.volume)/(self.volume + other.volume)) * other.velocity
-        self.velocity = new_velocity
-        other.velocity = new_velocity2
+        self.pos += self.velocity * self.acceleraton
 
     def apply_gravity(self):
-        self.direction.y += self.gravity
-        self.pos.y += self.direction.y
+        self.velocity.y += self.gravity
+        self.pos.y += self.velocity.y
 
     def check_collision(self):
-        if self.pos.x <= self.r: 
-            self.pos.x = self.r
-            self.direction.x = self.direction.x * -1
-        if self.pos.x + self.r >=WIDTH: 
-            self.direction.x = self.direction.x * -1
-            self.pos.x = WIDTH - self.r
-        if self.pos.y <= self.r: 
-            self.direction.y = self.direction.y * -1
-            self.pos.y = self.r
-        if self.pos.y + self.r >=HEIGHT: 
-            self.direction.y = self.direction.y * -1
-            self.pos.y = HEIGHT - self.r
+        #Check collision with walls
+        if self.pos.x - self.r < 0 or self.pos.x + self.r > SCREEN_WIDTH:
+            self.velocity.x *= -1
+        if self.pos.y - self.r < 0 or self.pos.y + self.r > SCREEN_HEIGHT:
+            self.velocity.y *= -1
 
+        #Check collision with other balls
         for ball in balls:
             if ball != self:
-                distance = math.hypot(ball.pos.x - self.pos.x, ball.pos.y - self.pos.y)
-
-                if distance <= (self.r + ball.r):
+                if (self.pos.distance_to(ball.pos) < self.r + ball.r):
+                    self.bounce(ball)
                     self.colour = GREEN
                     ball.colour = RED
-                    new_vector = ball.pos - self.pos
-                    new_vector2 = self.pos - ball.pos
-                    m1 = pygame.math.Vector2(self.direction.x, self.direction.y).reflect(new_vector)
-                    m2 = pygame.math.Vector2(ball.direction.x, ball.direction.y).reflect(new_vector2)
-                    self.direction.x, self.direction.y = m1.x, m1.y
-                    ball.direction.x, ball.direction.y = m2.x, m2.y
-                    
-                    self.bounce(ball)
+    
+    def bounce(self, other):
+        assert ( isinstance(other, Ball) )
+
+        distance = self.pos.distance_to(other.pos)
+        if distance < self.r + other.r:
+            # Separate the balls so they are no longer overlapping
+            overlap = self.r + other.r - distance
+            direction = (self.pos - other.pos).normalize()
+            self.pos += direction * (overlap / 2)
+            other.pos -= direction * (overlap / 2)
+
+            # Reflect velocities based on the normal
+            self_velocity_along_normal = self.velocity.dot(direction)
+            other_velocity_along_normal = other.velocity.dot(direction)
+
+            self.velocity -= direction * self_velocity_along_normal
+            other.velocity -= direction * other_velocity_along_normal
+
+            self.velocity += direction * other_velocity_along_normal
+            other.velocity += direction * self_velocity_along_normal
--- main.py
+++ main.py
@@ -3,32 +3,45 @@ from ball import Ball
 from settings import *
 import random
 
+# Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode(RES)
-clock = pygame.time.Clock()
 
-for ball in range(0,NUM_BALLS):
-    r = random.randint(10,40)
-    x = random.randint(r,WIDTH-r)
-    y = random.randint(r,HEIGHT-r)
-    
+def create_ball(x,y,r):
     ball = Ball(x,y,r,0,BLUE)
     balls.append(ball)
 
-run = True
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def main():
+    screen = pygame.display.set_mode(RES)
+    clock = pygame.time.Clock()
 
-    screen.fill('black')
+    for ball in range(0,NUM_BALLS):
+        r = random.randint(10,40)
+        x = random.randint(r,SCREEN_WIDTH-r)
+        y = random.randint(r,SCREEN_HEIGHT-r)
+        create_ball(x,y,r)
 
-    for ball in balls:
-        ball.move()
-        ball.check_collision()
-        ball.draw(screen)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            # Handle MOUSEBUTTONUP
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                create_ball(mouse_x,mouse_y, random.randint(10,40))
+
+        screen.fill('black')
+
+        for ball in balls:
+            ball.update()
+            ball.check_collision()
+            ball.draw(screen)
+        
+        pygame.display.flip()
+        clock.tick(60)
     
-    #pygame.time.delay(10)
-    pygame.display.update()
-    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
